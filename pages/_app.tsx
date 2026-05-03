000368e689ad4cff85ccacdf78cc443a2bc42d48
arielgabayyy-debug <arielgabayyy@gmail.com>
2026-05-03 23:08:19 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,4 @@
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import '../styles/globals.css'
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;700;900&family=Orbitron:wght@700;900&display=swap" rel="stylesheet" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
