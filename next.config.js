905c6b46c50a360e4309e70d9dbdce75d65d8336
arielgabayyy-debug <arielgabayyy@gmail.com>
2026-05-03 21:22:07 +0300

    Allow iframe embedding

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  typescript: {
-    ignoreBuildErrors: true
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
+          { key: 'Content-Security-Policy', value: "frame-ancestors 'self' https://nitukbeclick.co.il https://*.nitukbeclick.co.il *" },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+        ],
+      },
+    ]
   },
-  eslint: {
-    ignoreDuringBuilds: true
-  }
 }
 module.exports = nextConfig
